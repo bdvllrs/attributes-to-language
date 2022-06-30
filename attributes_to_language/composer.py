44fc895f7e605374ede8fffddb1b303a89b92296
Benjamin <bdvllrs@gmail.com>
2022-06-30 16:19:22 +0200

    Add modifiers

--- attributes_to_language/composer.py
+++ attributes_to_language/composer.py
@@ -4,35 +4,47 @@ import re
 
 
 class Composer:
-    def __init__(self, script_structures, triggers):
+    def __init__(self, script_structures, variants, available_writers, modifiers=None):
         """
         Args:
             script_structures:
-            triggers: List of triggers that will be called when a certain key is met in the structure
+            variants:
+            available_writers: Dict of list of writers for the attributes. The key of the dict corresponds to the attribute name
+                and the value is a list of type "Writer".
         """
         self.script_structures = script_structures
-        self.triggers = triggers
+        self.variants = variants
+        self.writers = available_writers
+        self.modifiers = modifiers
 
-    def __call__(self, data):
+    def __call__(self, attributes):
         """
-        Compose one sentence from a dict of data
+        Compose one sentence from a dict of attributes
         Args:
-            data: Dictionary where a key is an attribute name and the value is the value of the attribute that
+            attributes: Dictionary where a key is an attribute name and the value is the value of the attribute that
                 will be provided to the associated writer.
 
         Returns: The composed sentence.
         """
         # Select one of the templates
         selected_structure = random.choice(self.script_structures)
-        regex = r"{(.+?)}"
-        match = re.search(regex, selected_structure)
-        while match:
-            key = match.group(1)
-            has_triggered = False
-            for trigger in self.triggers:
-                if trigger.trigger == key:
-                    selected_structure = selected_structure.replace(match.group(), trigger(data))
-                    has_triggered = True
-                    break
-            match = re.search(regex, selected_structure) if has_triggered else False
-        return re.sub(' +', ' ', selected_structure).replace(" .", ".")
+        # Perform modifiers
+        if self.modifiers is not None:
+            for modifier in self.modifiers:
+                selected_structure = modifier(selected_structure)
+        # Decide which variants will be used
+        variants = dict()
+        for k, choices in self.variants.items():
+            variants[k] = random.choice(choices)
+        # Select a writer for each attribute and generate the str.
+        written_attrs = dict()
+        for attr_name, attr in attributes.items():
+            writer = random.choice(self.writers[attr_name])
+            if isinstance(attr, (list, tuple)):
+                written_attrs[attr_name] = writer(*attr).format(**variants)
+            else:
+                written_attrs[attr_name] = writer(attr).format(**variants)
+        # Create final caption
+        final_caption = selected_structure.format(**written_attrs, **variants).strip()
+        # remove multiple spaces and spaces in front of "."
+        return re.sub(' +', ' ', final_caption).replace(" .", ".")
