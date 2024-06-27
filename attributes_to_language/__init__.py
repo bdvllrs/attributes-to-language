308cc2be878ea63d9c8e79a551451cb511dec5fe
bdvllrs <bdvllrs@gmail.com>
2024-06-27 10:14:26 +0000

    Clean up and add classes to __init__

--- attributes_to_language/__init__.py
+++ attributes_to_language/__init__.py
@@ -1,5 +1,37 @@
+from attributes_to_language.composer import Composer
+from attributes_to_language.types import (
+    AttributeT,
+    CallbackVariantT,
+    Choices,
+    ChoicesT,
+    ComputedAttributeT,
+    VariantsT,
+    VariantT,
+)
 from attributes_to_language.version import __version__
+from attributes_to_language.writers import (
+    Bins2dWriter,
+    BinsWriter,
+    ContinuousAngleWriter,
+    OptionsWriter,
+    QuantizedWriter,
+    Writer,
+)
 
 __all__ = [
+    "Composer",
+    "AttributeT",
+    "CallbackVariantT",
+    "Choices",
+    "ChoicesT",
+    "ComputedAttributeT",
+    "VariantsT",
+    "VariantT",
+    "Bins2dWriter",
+    "BinsWriter",
+    "ContinuousAngleWriter",
+    "OptionsWriter",
+    "QuantizedWriter",
+    "Writer",
     "__version__",
 ]
